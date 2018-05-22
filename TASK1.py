fe9d798a4f98c12b089da2598da92264734c286e
PiezoDelRey <32424662+Stevenchaosz@users.noreply.github.com>
2018-05-22 14:36:53 +0800

    Add a input type validation
    Fix minor bugs; make the application more friendly due to new blank lines being added to the output

--- TASK1.py
+++ TASK1.py
@@ -4,23 +4,33 @@ midnight = []
 for i in range(0, 30):
 
     while True:
-        temperature_midday = input("Enter the temperature for midday.\n")
+        temperature_midday = raw_input("Enter the temperature for midday.\n")
+        try:
+            float(temperature_midday)
+        except ValueError:
+            print "This is not a number.\n"
+            continue
         temperature_midday_float = float(temperature_midday)
         if temperature_midday_float < -50 or temperature_midday_float > 60:
-            print("Sorry, your temperature is either too high or too low.")
+            print "Sorry, your temperature is either too high or too low."
         else:
             midday.append(temperature_midday)
-            print("Your temperature is recorded.")
+            print "Your temperature is recorded.\n"
             break
 
     while True:
-        temperature_midnight = input("Enter the temperature for midnight.\n")
+        temperature_midnight = raw_input("Enter the temperature for midnight.\n")
+        try:
+            float(temperature_midnight)
+        except ValueError:
+            print "This is not a number.\n"
+            continue
         temperature_midnight_float = float(temperature_midnight)
         if temperature_midnight_float < -50 or temperature_midnight_float > 60:
-            print("Sorry, your temperature is either too high or too low.")
+            print "Sorry, your temperature is either too high or too low."
         else:
             midnight.append(temperature_midnight)
-            print("Your temperature is recorded.")
+            print "Your temperature is recorded.\n"
             break
 
-print(midday, midnight)
+print midday, midnight
